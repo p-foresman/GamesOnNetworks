e33859172ec2392d65005b1def5e5b89f1a88457
p-foresman <98059656+p-foresman@users.noreply.github.com>
2023-03-17 17:51:55 -0700

    updates

--- analysis/plot_analysis.jl
+++ analysis/plot_analysis.jl
@@ -7,14 +7,14 @@ colors = [palette(:default)[11] palette(:default)[2] palette(:default)[2] palett
 
 test_plot = transitionTimesBoxPlot("./sqlite/SimulationSaves.sqlite", game_id=1, number_agents=10, memory_length=16, error=0.05, graph_ids=graph_ids, x_labels=x_labels, colors=colors, sample_size=20)
 
-legend_labels = ["Complete", "Erdos-Renyi λ=1"]
-colors = [palette(:default)[11], palette(:default)[2]]
-error_styles = [(:dash, nothing)]
-test_plot_2 = numberAgentsTransitionTimeLinePlot("./sqlite/SimulationSaves.sqlite"; game_id=1, number_agents_list=[10, 20, 30, 40], memory_length=10, errors=[0.1], graph_ids=[1, 2], sample_size=20, conf_intervals=true, legend_labels=legend_labels, colors=colors, error_styles=error_styles)
+legend_labels = ["Complete", "Erdos-Renyi λ=1", "Erdos-Renyi λ=5", "Small World"]
+colors = [palette(:default)[11], palette(:default)[2], palette(:default)[12], palette(:default)[9]]
+error_styles = [(:dash, nothing), (:solid, nothing)]
+test_plot_2 = numberAgentsTransitionTimeLinePlot("./sqlite/SlurmSimulationSaves.sqlite"; game_id=1, number_agents_list=[10, 20, 30, 40, 50, 60, 70], memory_length=10, errors=[0.05, 0.1], graph_ids=[1, 2, 3, 4], sample_size=19, conf_intervals=true, legend_labels=legend_labels, colors=colors, error_styles=error_styles)
 
 
 legend_labels = ["Complete", "Erdos-Renyi λ=1", "Erdos-Renyi λ=5", "Small World"]
 colors = [palette(:default)[11], palette(:default)[2], palette(:default)[12], palette(:default)[9]]
 error_styles = [(:dash, nothing), (:solid, nothing)]
-test_plot_3 = memoryLengthTransitionTimeLinePlot("./sqlite/SimulationSaves.sqlite"; game_id=1, number_agents=10, memory_length_list=[10, 13, 16, 19], errors=[0.05, 0.1], graph_ids=[1, 2, 3, 4], sample_size=20, conf_intervals=true, legend_labels=legend_labels, colors=colors, error_styles=error_styles)
+test_plot_3 = memoryLengthTransitionTimeLinePlot("./sqlite/SlurmSimulationSaves.sqlite"; game_id=1, number_agents=10, memory_length_list=[10, 13, 16, 19], errors=[0.05, 0.1], graph_ids=[1, 2, 3, 4], sample_size=19, conf_intervals=true, legend_labels=legend_labels, colors=colors, error_styles=error_styles)
 
