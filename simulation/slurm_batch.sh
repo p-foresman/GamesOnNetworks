88c97c06e88b658924bc077bf254f8b992d6d39c
p-foresman <98059656+p-foresman@users.noreply.github.com>
2023-06-26 15:52:14 -0700

    added slurm updates

---
+++ simulation/slurm_batch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=GamesOnNetworksBehavioral     # create a short name for your job
+#SBATCH --partition long
+#SBATCH --output=slurm-%A.%a.out # stdout file
+#SBATCH --error=slurm-%A.%a.err  # stderr file
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=20        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
+#SBATCH --array=0-4              # job array with index values 0, 1, 2, 3, 4
+#SBATCH --mail-type=all          # send email on job start, end and fault
+#SBATCH --mail-user=fores2@pdx.edu
+
+echo "My SLURM_ARRAY_JOB_ID is $SLURM_ARRAY_JOB_ID."
+echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
+echo "Executing on the machine:" $(hostname)
+
+julia ./run_simulation_slurm.jl
