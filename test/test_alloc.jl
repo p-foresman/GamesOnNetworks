f0596d3e4749110a28e38f4e8b137e54aa94aead
p-foresman <98059656+p-foresman@users.noreply.github.com>
2024-06-20 14:38:41 -0700

    test_alloc

---
+++ test/test_alloc.jl
@@ -0,0 +1,24 @@
+const payoff_matrix = Matrix{Tuple{Int8, Int*}}([(0, 0) (0, 0) (70, 30);
+                        (0, 0) (50, 50) (50, 30);
+                        (30, 70) (30, 50) (30, 30)])
+
+const game_list = [Game{3, 3}("Bargaining Game", payoff_matrix)]
+
+game = Game{3, 3}("Bargaining Game", payoff_matrix)
+
+sim_params = SimParams(1000, 10, 0.1, random_seed=1234)
+
+graph_params = ErdosRenyiParams(999.0)
+
+starting = FractiousState()
+
+stopping = EquityBehavioral(2)
+
+m = SimModel(game, sim_params, graph_params, starting, stopping)
+m3 = SimModel(game, sim_params, CompleteParams(), starting, stopping)
+
+
+function test_model(model::SimModel)
+    simulate(model, use_seed=true)
+    reset_model!(model)
+end
